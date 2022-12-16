1e4d314dd269e85f6beb142ad1805b546910c447
Artur_A <arthursae@mail.ru>
2022-12-16 22:17:09 +0500

    Initial commit

---
+++ task_001/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+void OutputResult(int[] numbers, int counter)
+{
+    Console.Write("Количество чисел больше нуля в массиве ");
+    Console.Write("[{0}]", string.Join(", ", numbers));
+    Console.Write(" -> " + counter);
+    Console.WriteLine(); // removes the trailing % symbol in the terminal
+}
+
+int CountNumbersAboveZero(int[] numbers)
+{
+    int counter = 0;
+    foreach (int number in numbers) if (number > 0) counter++;
+    return counter;
+}
+
+int[] CollectUserInputNumbersIntoArray(int numOfElements)
+{
+    int[] numbers = new int[numOfElements];
+
+    for (int i = 0; i < numOfElements; i++)
+    {
+        int order = i + 1;
+        Console.Write("Введите число " + order + " из " + numOfElements + ": ");
+        string userInput = Console.ReadLine();
+
+        if (Int32.TryParse(userInput, out _))
+        {
+            numbers[i] = Convert.ToInt32(userInput);
+        }
+        else
+        {
+            Console.WriteLine("Неправильный тип данных, повторите ввод!");
+            return CollectUserInputNumbersIntoArray(numOfElements);
+        }
+    }
+    return numbers;
+}
+
+string GetUserInputData(string msg)
+{
+    Console.Write(msg);
+    string userInput = Console.ReadLine();
+    return userInput;
+}
+
+int GetNumberOfElementsInArray(string msg)
+{
+    string userInput = GetUserInputData(msg);
+
+    if (Int32.TryParse(userInput, out _))
+    {
+        int numOfElements = Math.Abs(Convert.ToInt32(userInput));
+        return numOfElements > 0 ? numOfElements : GetNumberOfElementsInArray(msg);
+    } 
+    else
+    {
+        return GetNumberOfElementsInArray(msg);
+    }
+}
+
+Console.Clear();
+int numOfElements = GetNumberOfElementsInArray("Введите нужное количество элементов в массиве: ");
+int[] numbers = CollectUserInputNumbersIntoArray(numOfElements);
+int aboveZeroCounter = CountNumbersAboveZero(numbers);
+OutputResult(numbers, aboveZeroCounter);
---
+++ task_002/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void OutputIntersectionPointCoordinates(double[] intersectionPoint)
+{
+    Console.Write("Точка пересечения двух прямых: ");
+    Console.Write("[{0}]", string.Join(", ", intersectionPoint));
+    Console.WriteLine(); // removes the trailing % symbol in the terminal
+}
+
+double[] FindIntersectionPoint(double[,] linearFunctions)
+{
+    double[] intersectionPointXY = new double[2];
+    double b1 = linearFunctions[0,0];
+    double k1 = linearFunctions[0,1];
+    double b2 = linearFunctions[1,0];
+    double k2 = linearFunctions[1,1];
+    double x = (b2 - b1) / (k1 - k2);
+    intersectionPointXY[0] = x;
+    double y = k1 * x + b1;
+    intersectionPointXY[1] = y;
+    return intersectionPointXY;
+}
+
+bool IsParallel(double[,] linearFunctions)
+{
+    double k1 = linearFunctions[0,1];
+    double k2 = linearFunctions[1,1];
+    return (k1 - k2 == 0);
+}
+
+double[,] GetSlopeAndYInterceptData()
+{
+    double[,] linearFunctions = new double[2,2];
+
+    for (int i = 0; i < 2; i++)
+    {
+        int order = i + 1;
+
+        for (int j = 0; j < 2; j++)
+        {
+            string id = (j == 0) ? "b" : "k";
+            Console.Write("Введите коэффициент " + id + order + " для " + order + "-й прямой: ");
+            string userInput = Console.ReadLine();
+
+            if (Int32.TryParse(userInput, out _))
+            {
+                linearFunctions[i, j] = Convert.ToInt32(userInput);
+            }
+            else
+            {
+                Console.WriteLine("Неправильный тип данных, повторите ввод!");
+                return GetSlopeAndYInterceptData();
+            }
+        }
+    }
+
+    return linearFunctions;
+}
+
+Console.Clear();
+double[,] linearFunctions = new double[2,2];
+linearFunctions = GetSlopeAndYInterceptData();
+if (!IsParallel(linearFunctions))
+{
+    double[] intersectionPoint = new double[2];
+    intersectionPoint = FindIntersectionPoint(linearFunctions);
+    OutputIntersectionPointCoordinates(intersectionPoint);
+}
+else
+{
+    Console.WriteLine("Прямые не пересекаются");
+}
